6b70ad371942bbe0d4ec1ee2aeda7cd7315866ed
Sergii Moroz <sergii.moroz@mail.de>
2025-09-11 13:23:51 +0200

    add shadcn components: button, separator

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,6 +20,10 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        sidebarItem:
+          "gap-2 justify-start hover:underline decoration-emerald-500 decoration-2",
+        sidebarActiveItem:
+          "gap-2 justify-start bg-primary text-primary-foreground hover:bg-primary/90",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
