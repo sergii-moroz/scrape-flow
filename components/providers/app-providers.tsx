5d4f90b07e626b37a932b9c8883cdbeeacbe688d
Sergii Moroz <sergii.moroz@mail.de>
2025-09-11 20:41:08 +0200

    add tanstack provider

--- components/providers/app-providers.tsx
+++ components/providers/app-providers.tsx
@@ -1,20 +1,27 @@
 "use client"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "next-themes"
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export function AppProviders({
 	children
 } : {
 	children: ReactNode
 }) {
+	const [queryClient] = useState(() => new QueryClient())
+
 	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="system"
-			enableSystem
-		>
-			{children}
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+			>
+				{children}
+			</ThemeProvider>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	)
 }
